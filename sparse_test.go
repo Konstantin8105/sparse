a7f796833e96dfbd17f28988a462090254402180
Konstantin8105 <konstantin8105@gmail.com>
2018-11-16 23:51:23 +0300

    Test: refactoring

--- sparse_test.go
+++ sparse_test.go
@@ -10,43 +10,47 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func buildC(t *testing.T, filename string) {
+	// CSparse C source
+	csFiles, err := filepath.Glob("CSparse/Source/" + "*.c")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// build testdata application :
+	//
+	// clang -ICSparse/Include/   \
+	//  ./CSparse/Source/*.c      \
+	//  ./testdata/csparse_test.c \
+	//  -lm                       \
+	//  -o                        \
+	//  ./testdata/csparse_test
+	var args []string
+	args = append(args, "-ICSparse/Include/")
+	args = append(args, csFiles...)
+	args = append(args, filename)
+	args = append(args, "-lm")
+	args = append(args, "-o")
+	args = append(args, "testdata/csparse_test")
+
+	cmd := exec.Command("clang", args...)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	if err != nil {
+		t.Fatalf("cmd.Run() failed with %s.\n%s\n%s\n",
+			err,
+			stderr.String(),
+			stdout.String(),
+		)
+	}
+}
+
+func TestDemo1(t *testing.T) {
 
 	t.Run("Build test", func(t *testing.T) {
-		// CSparse C source
-		csFiles, err := filepath.Glob("CSparse/Source/" + "*.c")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// build testdata application :
-		//
-		// clang -ICSparse/Include/   \
-		//  ./CSparse/Source/*.c      \
-		//  ./testdata/csparse_test.c \
-		//  -lm                       \
-		//  -o                        \
-		//  ./testdata/csparse_test
-		var args []string
-		args = append(args, "-ICSparse/Include/")
-		args = append(args, csFiles...)
-		args = append(args, "testdata/csparse_test.c")
-		args = append(args, "-lm")
-		args = append(args, "-o")
-		args = append(args, "testdata/csparse_test")
-
-		cmd := exec.Command("clang", args...)
-		var stdout, stderr bytes.Buffer
-		cmd.Stdout = &stdout
-		cmd.Stderr = &stderr
-		err = cmd.Run()
-		if err != nil {
-			t.Fatalf("cmd.Run() failed with %s.\n%s\n%s\n",
-				err,
-				stderr.String(),
-				stdout.String(),
-			)
-		}
+		buildC(t, "testdata/csparse_demo1_test.c")
 	})
 
 	matrixes, err := filepath.Glob("CSparse/Matrix/" + "*")
@@ -61,7 +65,7 @@ func Test(t *testing.T) {
 			continue
 		}
 
-		t.Run(matrixes[i], func(t *testing.T) {
+		t.Run("Demo1: "+matrixes[i], func(t *testing.T) {
 			// data checking
 			cmd := exec.Command(
 				"./testdata/csparse_test",
