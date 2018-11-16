09f5dfe7a1dcee19c59387243b2ca6d28b09b69c
Konstantin8105 <konstantin8105@gmail.com>
2018-11-17 00:15:21 +0300

    Test: prepare demo2 test

---
+++ testdata/csparse_demo2_test.c
@@ -0,0 +1,98 @@
+#include "cs.h"
+#include <time.h>
+
+typedef struct problem_struct
+{
+    cs *A ;
+    cs *C ;
+    csi sym ;
+    double *x ;
+    double *b ;
+    double *resid ;
+} problem ;
+
+/* 1 if A is square & upper tri., -1 if square & lower tri., 0 otherwise */
+static csi is_sym (cs *A)
+{
+    csi is_upper, is_lower, j, p, n = A->n, m = A->m, *Ap = A->p, *Ai = A->i ;
+    if (m != n) return (0) ;
+    is_upper = 1 ;
+    is_lower = 1 ;
+    for (j = 0 ; j < n ; j++)
+    {
+        for (p = Ap [j] ; p < Ap [j+1] ; p++)
+        {
+            if (Ai [p] > j) is_upper = 0 ;
+            if (Ai [p] < j) is_lower = 0 ;
+        }
+    }
+    return (is_upper ? 1 : (is_lower ? -1 : 0)) ;
+}
+
+/* true for off-diagonal entries */
+static csi dropdiag (csi i, csi j, double aij, void *other) { return (i != j) ;}
+
+/* C = A + triu(A,1)' */
+static cs *make_sym (cs *A)
+{
+    cs *AT, *C ;
+    AT = cs_transpose (A, 1) ;          /* AT = A' */
+    cs_fkeep (AT, &dropdiag, NULL) ;    /* drop diagonal entries from AT */
+    C = cs_add (A, AT, 1, 1) ;          /* C = A+AT */
+    cs_spfree (AT) ;
+    return (C) ;
+}
+
+/* free a problem */
+problem *free_problem (problem *Prob)
+{
+    if (!Prob) return (NULL) ;
+    cs_spfree (Prob->A) ;
+    if (Prob->sym) cs_spfree (Prob->C) ;
+    cs_free (Prob->b) ;
+    cs_free (Prob->x) ;
+    cs_free (Prob->resid) ;
+    return (cs_free (Prob)) ;
+}
+
+/* read a problem from a file; use %g for integers to avoid csi conflicts */
+problem *get_problem (FILE *f, double tol)
+{
+    cs *T, *A, *C ;
+    csi sym, m, n, mn, nz1, nz2 ;
+    problem *Prob ;
+    Prob = cs_calloc (1, sizeof (problem)) ;
+    if (!Prob) return (NULL) ;
+    T = cs_load (f) ;                   /* load triplet matrix T from a file */
+    Prob->A = A = cs_compress (T) ;     /* A = compressed-column form of T */
+    cs_spfree (T) ;                     /* clear T */
+    if (!cs_dupl (A)) return (free_problem (Prob)) ; /* sum up duplicates */
+    Prob->sym = sym = is_sym (A) ;      /* determine if A is symmetric */
+    m = A->m ; n = A->n ;
+    mn = CS_MAX (m,n) ;
+    nz1 = A->p [n] ;
+    cs_dropzeros (A) ;                  /* drop zero entries */
+    nz2 = A->p [n] ;
+    if (tol > 0) cs_droptol (A, tol) ;  /* drop tiny entries (just to test) */
+    Prob->C = C = sym ? make_sym (A) : A ;  /* C = A + triu(A,1)', or C=A */
+    if (!C) return (free_problem (Prob)) ;
+    printf ("\n--- Matrix: %g-by-%g, nnz: %g (sym: %g: nnz %g), norm: %8.2e\n",
+            (double) m, (double) n, (double) (A->p [n]), (double) sym,
+            (double) (sym ? C->p [n] : 0), cs_norm (C)) ;
+    if (nz1 != nz2) printf ("zero entries dropped: %g\n", (double) (nz1 - nz2));
+    if (nz2 != A->p [n]) printf ("tiny entries dropped: %g\n",
+            (double) (nz2 - A->p [n])) ;
+    Prob->b = cs_malloc (mn, sizeof (double)) ;
+    Prob->x = cs_malloc (mn, sizeof (double)) ;
+    Prob->resid = cs_malloc (mn, sizeof (double)) ;
+    return ((!Prob->b || !Prob->x || !Prob->resid) ? free_problem (Prob) : Prob) ;
+}
+
+// cs_demo2: read a matrix and solve a linear system
+int main (void)
+{
+    problem *Prob = get_problem (stdin, 1e-14) ;
+    // demo2 (Prob) ;
+    free_problem (Prob) ;
+    return (0) ;
+}
