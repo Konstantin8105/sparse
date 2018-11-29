1deb68d661a10a99ec0e3a2f4dea853e07d064a7
Konstantin8105 <konstantin8105@gmail.com>
2018-11-29 09:59:51 +0300

    clean

--- operations.go
+++ operations.go
@@ -53,39 +53,3 @@ func IsSingular(A *Matrix) (bool, error) {
 
 	return false, nil
 }
-
-// IsSymmetrical return true is square matrix A is symmetrical
-func IsSymmetrical(A *Matrix) (bool, error) {
-	// check input data
-	et := errors.New("Function IsSingular: check input data")
-	if A == nil {
-		_ = et.Add(fmt.Errorf("matrix A is nil"))
-	}
-	if A != nil && A.nz != -1 {
-		_ = et.Add(fmt.Errorf("matrix A is not CSC(Compressed Sparse Column) format"))
-	}
-	if A != nil {
-		if A.n != A.m {
-			_ = et.Add(fmt.Errorf("matrix A is not square: %d != %d", A.n, A.m))
-		}
-	}
-
-	if et.IsError() {
-		return true, et
-	}
-
-	// initialization
-	n, Ap, Ai, Ax := A.n, A.p, A.i, A.x
-
-	_ = Ai
-	_ = Ax
-
-	// calculation
-	for j := 0; j < n; j++ {
-		for p := Ap[j]; p < Ap[j+1]; p++ {
-			// TODO
-		}
-	}
-
-	return false, nil
-}
