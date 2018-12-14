32d33da08d53dac43e0de24f3d2551ee6b2644e8
Konstantin8105 <konstantin8105@gmail.com>
2018-12-14 15:06:51 +0300

    change location of comments

--- eispack_test.go
+++ eispack_test.go
@@ -7,48 +7,99 @@ import (
 	"testing"
 )
 
+func TestEispack(t *testing.T) {
+	// compile eispack
+
+	// gcc -o eispack eispack_prb1.c eispack.c eispack.h -lm
+	// ./eispack
+
+	// generate output
+
+	// run transpiled code
+
+	// compare output
+}
+
 // r8mat_print - transpiled function from  $GOPATH/src/github.com/Konstantin8105/sparse/Eispack/eispack.c:1307
 //
+//
+//
+//  Purpose:
+//
+//    R8MAT_PRINT prints an R8MAT.
+//
+//  Discussion:
+//
+//    An R8MAT is a doubly dimensioned array of R8's, which
+//    may be stored as a vector in column-major order.
+//
+//    Entry A(I,J) is stored as A[I+J*M]
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    28 May 2008
+//
+//  Author:
+//
+//    John Burkardt
+//
+//  Parameters:
+//
+//    Input, int M, the number of rows in A.
+//
+//    Input, int N, the number of columns in A.
+//
+//    Input, double A[M*N], the M by N matrix.
+//
+//    Input, char *TITLE, a title.
+//
 func r8mat_print(m int, n int, a []float64, title string) {
-	//
-	//
-	//  Purpose:
-	//
-	//    R8MAT_PRINT prints an R8MAT.
-	//
-	//  Discussion:
-	//
-	//    An R8MAT is a doubly dimensioned array of R8's, which
-	//    may be stored as a vector in column-major order.
-	//
-	//    Entry A(I,J) is stored as A[I+J*M]
-	//
-	//  Licensing:
-	//
-	//    This code is distributed under the GNU LGPL license.
-	//
-	//  Modified:
-	//
-	//    28 May 2008
-	//
-	//  Author:
-	//
-	//    John Burkardt
-	//
-	//  Parameters:
-	//
-	//    Input, int M, the number of rows in A.
-	//
-	//    Input, int N, the number of columns in A.
-	//
-	//    Input, double A[M*N], the M by N matrix.
-	//
-	//    Input, char *TITLE, a title.
-	//
 	r8mat_print_some(m, n, a, 1, 1, m, n, title)
 }
 
 // r8mat_print_some - transpiled function from  $GOPATH/src/github.com/Konstantin8105/sparse/Eispack/eispack.c:1351
+//
+//
+//
+//  Purpose:
+//
+//    R8MAT_PRINT_SOME prints some of an R8MAT.
+//
+//  Discussion:
+//
+//    An R8MAT is a doubly dimensioned array of R8's, which
+//    may be stored as a vector in column-major order.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    20 August 2010
+//
+//  Author:
+//
+//    John Burkardt
+//
+//  Parameters:
+//
+//    Input, int M, the number of rows of the matrix.
+//    M must be positive.
+//
+//    Input, int N, the number of columns of the matrix.
+//    N must be positive.
+//
+//    Input, double A[M*N], the matrix.
+//
+//    Input, int ILO, JLO, IHI, JHI, designate the first row and
+//    column, and the last row and column to be printed.
+//
+//    Input, char *TITLE, a title.
 //
 func r8mat_print_some(m int, n int, a []float64, ilo int, jlo int, ihi int, jhi int, title string) {
 	var i int
@@ -57,44 +108,6 @@ func r8mat_print_some(m int, n int, a []float64, ilo int, jlo int, ihi int, jhi 
 	var j int
 	var j2hi int
 	var j2lo int
-	//
-	//
-	//  Purpose:
-	//
-	//    R8MAT_PRINT_SOME prints some of an R8MAT.
-	//
-	//  Discussion:
-	//
-	//    An R8MAT is a doubly dimensioned array of R8's, which
-	//    may be stored as a vector in column-major order.
-	//
-	//  Licensing:
-	//
-	//    This code is distributed under the GNU LGPL license.
-	//
-	//  Modified:
-	//
-	//    20 August 2010
-	//
-	//  Author:
-	//
-	//    John Burkardt
-	//
-	//  Parameters:
-	//
-	//    Input, int M, the number of rows of the matrix.
-	//    M must be positive.
-	//
-	//    Input, int N, the number of columns of the matrix.
-	//    N must be positive.
-	//
-	//    Input, double A[M*N], the matrix.
-	//
-	//    Input, int ILO, JLO, IHI, JHI, designate the first row and
-	//    column, and the last row and column to be printed.
-	//
-	//    Input, char *TITLE, a title.
-	//
 	fmt.Fprintf(os.Stdout, "\n")
 	fmt.Fprintf(os.Stdout, "%s\n", title)
 	if m <= 0 || n <= 0 {
@@ -144,71 +157,71 @@ func r8mat_print_some(m int, n int, a []float64, ilo int, jlo int, ihi int, jhi 
 
 // r8mat_uniform_01_new - transpiled function from  $GOPATH/src/github.com/Konstantin8105/sparse/Eispack/eispack.c:1459
 //
+//
+//
+//  Purpose:
+//
+//    R8MAT_UNIFORM_01_NEW fills an R8MAT with pseudorandom values scaled to [0,1].
+//
+//  Discussion:
+//
+//    An R8MAT is a doubly dimensioned array of R8 values, stored as a vector
+//    in column-major order.
+//
+//    This routine implements the recursion
+//
+//      seed = 16807 * seed mod ( 2^31 - 1 )
+//      unif = seed / ( 2^31 - 1 )
+//
+//    The integer arithmetic never requires more than 32 bits,
+//    including a sign bit.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    30 June 2009
+//
+//  Author:
+//
+//    John Burkardt
+//
+//  Reference:
+//
+//    Paul Bratley, Bennett Fox, Linus Schrage,
+//    A Guide to Simulation,
+//    Springer Verlag, pages 201-202, 1983.
+//
+//    Bennett Fox,
+//    Algorithm 647:
+//    Implementation and Relative Efficiency of Quasirandom
+//    Sequence Generators,
+//    ACM Transactions on Mathematical Software,
+//    Volume 12, Number 4, pages 362-376, 1986.
+//
+//    Philip Lewis, Allen Goodman, James Miller,
+//    A Pseudo-Random Number Generator for the System/360,
+//    IBM Systems Journal,
+//    Volume 8, pages 136-143, 1969.
+//
+//  Parameters:
+//
+//    Input, int M, N, the number of rows and columns.
+//
+//    Input/output, int *SEED, the "seed" value.  Normally, this
+//    value should not be 0, otherwise the output value of SEED
+//    will still be 0, and R8_UNIFORM will be 0.  On output, SEED has
+//    been updated.
+//
+//    Output, double R8MAT_UNIFORM_01_NEW[M*N], a matrix of pseudorandom values.
+//
 func r8mat_uniform_01_new(m int, n int, seed *int) []float64 {
 	var i int
 	var j int
 	var k int
 	var r []float64
-	//
-	//
-	//  Purpose:
-	//
-	//    R8MAT_UNIFORM_01_NEW fills an R8MAT with pseudorandom values scaled to [0,1].
-	//
-	//  Discussion:
-	//
-	//    An R8MAT is a doubly dimensioned array of R8 values, stored as a vector
-	//    in column-major order.
-	//
-	//    This routine implements the recursion
-	//
-	//      seed = 16807 * seed mod ( 2^31 - 1 )
-	//      unif = seed / ( 2^31 - 1 )
-	//
-	//    The integer arithmetic never requires more than 32 bits,
-	//    including a sign bit.
-	//
-	//  Licensing:
-	//
-	//    This code is distributed under the GNU LGPL license.
-	//
-	//  Modified:
-	//
-	//    30 June 2009
-	//
-	//  Author:
-	//
-	//    John Burkardt
-	//
-	//  Reference:
-	//
-	//    Paul Bratley, Bennett Fox, Linus Schrage,
-	//    A Guide to Simulation,
-	//    Springer Verlag, pages 201-202, 1983.
-	//
-	//    Bennett Fox,
-	//    Algorithm 647:
-	//    Implementation and Relative Efficiency of Quasirandom
-	//    Sequence Generators,
-	//    ACM Transactions on Mathematical Software,
-	//    Volume 12, Number 4, pages 362-376, 1986.
-	//
-	//    Philip Lewis, Allen Goodman, James Miller,
-	//    A Pseudo-Random Number Generator for the System/360,
-	//    IBM Systems Journal,
-	//    Volume 8, pages 136-143, 1969.
-	//
-	//  Parameters:
-	//
-	//    Input, int M, N, the number of rows and columns.
-	//
-	//    Input/output, int *SEED, the "seed" value.  Normally, this
-	//    value should not be 0, otherwise the output value of SEED
-	//    will still be 0, and R8_UNIFORM will be 0.  On output, SEED has
-	//    been updated.
-	//
-	//    Output, double R8MAT_UNIFORM_01_NEW[M*N], a matrix of pseudorandom values.
-	//
 	r = make([]float64, uint32(m*n)*8*1/8)
 	for j = 0; j < n; j++ {
 		for i = 0; i < m; i++ {
@@ -225,38 +238,38 @@ func r8mat_uniform_01_new(m int, n int, seed *int) []float64 {
 
 // r8vec_print - transpiled function from  $GOPATH/src/github.com/Konstantin8105/sparse/Eispack/eispack.c:1549
 //
+//
+//
+//  Purpose:
+//
+//    R8VEC_PRINT prints an R8VEC.
+//
+//  Discussion:
+//
+//    An R8VEC is a vector of R8's.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    08 April 2009
+//
+//  Author:
+//
+//    John Burkardt
+//
+//  Parameters:
+//
+//    Input, int N, the number of components of the vector.
+//
+//    Input, double A[N], the vector to be printed.
+//
+//    Input, char *TITLE, a title.
+//
 func r8vec_print(n int, a []float64, title string) {
 	var i int
-	//
-	//
-	//  Purpose:
-	//
-	//    R8VEC_PRINT prints an R8VEC.
-	//
-	//  Discussion:
-	//
-	//    An R8VEC is a vector of R8's.
-	//
-	//  Licensing:
-	//
-	//    This code is distributed under the GNU LGPL license.
-	//
-	//  Modified:
-	//
-	//    08 April 2009
-	//
-	//  Author:
-	//
-	//    John Burkardt
-	//
-	//  Parameters:
-	//
-	//    Input, int N, the number of components of the vector.
-	//
-	//    Input, double A[N], the vector to be printed.
-	//
-	//    Input, char *TITLE, a title.
-	//
 	fmt.Fprintf(os.Stdout, "\n")
 	fmt.Fprintf(os.Stdout, "%s\n", title)
 	fmt.Fprintf(os.Stdout, "\n")
@@ -287,7 +300,7 @@ func r8vec_print(n int, a []float64, title string) {
 //
 //    John Burkardt
 //
-func TestEispack(t *testing.T) {
+func intenalEispackTest(t *testing.T) {
 	// timestamp()
 	fmt.Fprintf(os.Stdout, "\n")
 	fmt.Fprintf(os.Stdout, "EISPACK_PRB\n")
@@ -399,6 +412,24 @@ func test06() {
 
 // test065 - transpiled function from  $GOPATH/src/github.com/Konstantin8105/sparse/Eispack/eispack_prb1.c:187
 //
+//
+//
+//  Purpose:
+//
+//    TEST065 tests RS.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    11 November 2012
+//
+//  Author:
+//
+//    John Burkardt
+//
 func test065() {
 	var a []float64
 	var a2 []float64 = make([]float64, 9)
@@ -413,24 +444,6 @@ func test065() {
 	var t float64
 	var w []float64 = make([]float64, 3)
 	var x []float64 = make([]float64, 9)
-	//
-	//
-	//  Purpose:
-	//
-	//    TEST065 tests RS.
-	//
-	//  Licensing:
-	//
-	//    This code is distributed under the GNU LGPL license.
-	//
-	//  Modified:
-	//
-	//    11 November 2012
-	//
-	//  Author:
-	//
-	//    John Burkardt
-	//
 	fmt.Fprintf(os.Stdout, "\n")
 	fmt.Fprintf(os.Stdout, "TEST065\n")
 	fmt.Fprintf(os.Stdout, "  RS computes the eigenvalues and eigenvectors\n")
@@ -482,6 +495,24 @@ func test065() {
 
 // test07 - transpiled function from  $GOPATH/src/github.com/Konstantin8105/sparse/Eispack/eispack_prb1.c:293
 //
+//
+//
+//  Purpose:
+//
+//    TEST07 tests RSB.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    12 November 2012
+//
+//  Author:
+//
+//    John Burkardt
+//
 func test07() {
 	var a []float64
 	var a2 []float64
@@ -494,24 +525,6 @@ func test07() {
 	var r []float64
 	var w []float64
 	var x []float64
-	//
-	//
-	//  Purpose:
-	//
-	//    TEST07 tests RSB.
-	//
-	//  Licensing:
-	//
-	//    This code is distributed under the GNU LGPL license.
-	//
-	//  Modified:
-	//
-	//    12 November 2012
-	//
-	//  Author:
-	//
-	//    John Burkardt
-	//
 	a = make([]float64, uint32(n*mb)*8*1/8)
 	for j = 0; j < mb; j++ {
 		for i = 0; i < n; i++ {
