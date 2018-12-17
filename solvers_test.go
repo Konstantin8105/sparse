684fd39b21556fdcf598a10acf9814c46e8b8b69
Konstantin8105 <konstantin8105@gmail.com>
2018-12-17 10:19:04 +0300

    add test for amd

--- solvers_test.go
+++ solvers_test.go
@@ -651,36 +651,44 @@ func TestMatLU(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	for i := range matrixes {
-		if testing.Short() && i < len(matrixes)-1 {
-			continue
+	for _, amd := range []sparse.Order{
+		sparse.AmdNatural,
+		sparse.AmdChol,
+		sparse.AmdLU,
+		sparse.AmdQR,
+	} {
+		for i := range matrixes {
+			if testing.Short() && i < len(matrixes)-1 {
+				continue
+			}
+			t.Run(matrixes[i]+"/"+amd.String(), func(t *testing.T) {
+				o, err := ioutil.ReadFile(matrixes[i])
+				if err != nil {
+					t.Fatal(err)
+				}
+				var stdin bytes.Buffer
+				stdin.Write(o)
+				T, err := sparse.Load(&stdin)
+				if err != nil {
+					t.Fatal(err)
+				}
+				A, err := sparse.Compress(T)
+				if err != nil {
+					t.Fatal(err)
+				}
+				r, c := A.Dims()
+				t.Logf("size : %4d %4d", r, c)
+				if ok, err := sparse.IsSym(A); !ok || err != nil {
+					t.Fatalf("matrix is not symmetrical: %v", err)
+				}
+				t.Log("symmetrical")
+				lu := new(sparse.LU)
+				lu.Order(amd)
+				err = lu.Factorize(A)
+				if err != nil {
+					t.Fatal(err)
+				}
+			})
 		}
-		t.Run(matrixes[i], func(t *testing.T) {
-			o, err := ioutil.ReadFile(matrixes[i])
-			if err != nil {
-				t.Fatal(err)
-			}
-			var stdin bytes.Buffer
-			stdin.Write(o)
-			T, err := sparse.Load(&stdin)
-			if err != nil {
-				t.Fatal(err)
-			}
-			A, err := sparse.Compress(T)
-			if err != nil {
-				t.Fatal(err)
-			}
-			r, c := A.Dims()
-			t.Logf("size : %4d %4d", r, c)
-			if ok, err := sparse.IsSym(A); !ok || err != nil {
-				t.Fatalf("matrix is not symmetrical: %v", err)
-			}
-			t.Log("symmetrical")
-			lu := new(sparse.LU)
-			err = lu.Factorize(A)
-			if err != nil {
-				t.Fatal(err)
-			}
-		})
 	}
 }
